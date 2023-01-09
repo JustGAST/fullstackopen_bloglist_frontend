15796dfb06333fb41ff55fe8e938c4742e507355
justgast <justgast@gmail.com>
2023-01-09 13:27:38 +0300

    Part 5: Exercise 5.7 ready;

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
+import {useState} from 'react';
+
 function Blog({ blog }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const blogStyle = {
+    padding: '5px',
+    border: '1px solid black',
+    margin: '5px 0'
+  };
+
+  console.log(blog);
+
   return (
-    <div>
+    <div style={blogStyle}>
       {blog.title} by {blog.author}
+      <button onClick={() => setExpanded(!expanded)}>{expanded ? 'Hide' : 'View'}</button>
+      {expanded && (
+        <>
+          <div>{blog.url}</div>
+          <div>Likes: {blog.likes} <button>Like</button></div>
+          <div>{blog.user && blog.user.name}</div>
+        </>
+      )}
     </div>
   );
 }

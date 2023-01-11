148dcefdf013a427f28851792ffb6635c90916bb
justgast <justgast@gmail.com>
2023-01-11 14:08:13 +0300

    Part 5: Exercise 5.18 ready;

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -14,12 +14,39 @@ describe('Blog app', () => {
     cy.contains('Blogs')
   })
 
-  it('login form can be accessed', function () {
+  it('login form is shown', function () {
     cy.contains('Login').click()
     cy.contains('Login to application')
     cy.get('#login-form')
       .get('input[name=username]').should('exist')
       .get('input[name=password]').should('exist')
       .get('button[type=submit]').should('exist')
+  })
+
+  describe('Login', function() {
+    it ('successful with valid credentials', function() {
+      cy.contains('Login').click()
+      cy.get('input[name=username]').type('test_user')
+      cy.get('input[name=password]').type('123456')
+      cy.get('button[type=submit]').click()
+
+      cy.contains('Test User logged in')
+      cy.contains('Log out')
+    })
+
+    it ('unsuccessful with invalid credentials', function() {
+      cy.contains('Login').click()
+
+      cy.get('input[name=username]').type('wrong_user')
+      cy.get('input[name=password]').type('654321')
+      cy.get('button[type=submit]').click()
+
+      cy.contains('incorrect username or password')
+      cy.get('.notification.danger')
+        .should('exist')
+        .and('have.css', 'background-color', 'rgb(255, 200, 200)')
+        .and('have.css', 'color', 'rgb(139, 0, 0)')
+        .and('have.css', 'border-color', 'rgb(139, 0, 0)')
+    })
   })
 })
